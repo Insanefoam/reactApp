5f3e44486ff7cc04e4e5c8f14e73c8b13291e3be
Daniil <spartaq123456789@gmail.com>
2020-02-13 21:22:57 +0600

    13.02 last commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-ReactDom.render(<h1>Hello World!</h1>,document.getElementById("root"));
+function MyApp(){
+    return (
+    <div>
+        <h1>Hello, my name is Daniil</h1>
+        <p>I'm 20 years old and i'm a student. I learning in OmSTU</p>
+        <ol>
+            Languages that I learned
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ol>
+    </div>
+    )
+}
+
+ReactDom.render(<MyApp />, document.getElementById("root"));
