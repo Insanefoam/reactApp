f095f06f67a9fca212855cc869e604fb12a7b282
Daniil <spartaq123456789@gmail.com>
2020-02-14 15:44:38 +0600

    add components dir

---
+++ src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function MyInfo(){
+    return (
+    <div>
+        <h1>Hello, my name is Daniil</h1>
+        <p>I'm 20 years old and i'm a student. I learning in OmSTU</p>
+        <ol>
+            Languages that I learned
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ol>
+    </div>
+    )
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-function MyApp(){
-    return (
-    <div>
-        <h1>Hello, my name is Daniil</h1>
-        <p>I'm 20 years old and i'm a student. I learning in OmSTU</p>
-        <ol>
-            Languages that I learned
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-        </ol>
-    </div>
-    )
-}
+import MyInfo from "./components/MyInfo"
 
-ReactDom.render(<MyApp />, document.getElementById("root"));
+ReactDom.render(<MyInfo />, document.getElementById("root"));
