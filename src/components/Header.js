7e5508a636f5c08a0fa6e85f419c9e4bf2f9d1f3
Daniil <spartaq123456789@gmail.com>
2020-02-14 16:27:05 +0600

    3nth task (nested components)

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src="https://cdn.worldvectorlogo.com/logos/tiktok-logo.svg" width="45px" height="45px" alt="Logo"></img>
+            <nav>
+                <ul>
+                    <a href=""><li>link 1</li></a>
+                    <a href=""><li>link 2</li></a>
+                    <a href=""><li>link 3</li></a>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-import MyInfo from "./components/MyInfo"
+import App from "./components/App"
 
-ReactDom.render(<MyInfo />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
