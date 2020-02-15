e528bbaf061d0900faec8cf31518689fdcf285ae
Daniil <spartaq123456789@gmail.com>
2020-02-15 19:54:34 +0600

    javascript in jsx

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 function MainContent(){
+    let timeOfDay = null;
+    let date = new Date();
+    let hours = date.getHours();
+    if(hours < 6){
+        timeOfDay = "morning";
+    }else if(hours < 21){
+        timeOfDay = "day";
+    }else{
+        timeOfDay = "night";
+    }
     return(
         <main>
+            <div>Hello, user! Good {timeOfDay}. </div>
             <span>Todo list:</span>
             <ol>
                 <li>First todo <input type="checkbox"/></li>
