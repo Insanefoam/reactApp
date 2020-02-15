8ce652dc7bbbdc66e38399a396e9702888b7e8db
Daniil <spartaq123456789@gmail.com>
2020-02-15 19:49:15 +0600

    add some remarks

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(){
     return(
         <header>
             <img src="https://cdn.worldvectorlogo.com/logos/tiktok-logo.svg" alt="Logo" class="logo__image"></img>
-            <span class="logo__name">Todo site</span>
+            <span class="logo__name"><a href="">Todo site</a></span>
             <nav class="navbar">
                 <ul class="navbar__wraper">
                     <li class="navbar__element"><a href="">Main</a></li>
