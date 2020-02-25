2c5328337df7ba73044fb0f8ed7ae5a415238f29
Daniil <spartaq123456789@gmail.com>
2020-02-25 11:53:31 +0600

    add state to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react"
 
 class Header extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            logoText: "Todo site"
+        };
+    }
+
     render() {
         return(
             <header>
                 <img src="https://cdn.worldvectorlogo.com/logos/tiktok-logo.svg" alt="Logo" class="logo__image"></img>
-                <span class="logo__name"><a href="">Todo site</a></span>
+                <span class="logo__name"><a href="">{this.state.logoText}</a></span>
                 <nav class="navbar">
                     <ul class="navbar__wraper">
                         <li class="navbar__element"><a href="">Main</a></li>
